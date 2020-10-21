32587825af369146df621c4323d79cd0bed08360
eru <eru160@protonmail.com>
2020-10-21 19:49:11 +0900

    パターン
    スタイルも少しいじった

--- src/index.tsx
+++ src/index.tsx
@@ -28,17 +28,6 @@ const Cell: React.SFC<CellProps> = ({
 
   return (
     <div
-      className="cell"
-      style={{
-        background,
-        border: "1px solid #000",
-        padding: 0,
-        width: cellSize,
-        height: cellSize,
-        lineHeight: cellSize,
-        marginTop: -1,
-        marginRight: -1,
-      }}
       onMouseDown={(e) => {
         switch (e.nativeEvent.which) {
           case 1:
@@ -52,21 +41,31 @@ const Cell: React.SFC<CellProps> = ({
         }
       }}
       onMouseEnter={() => handleMouseEnter()}
+      style={{
+        background,
+        border: "1px solid #000",
+        padding: 0,
+        width: cellSize,
+        height: cellSize,
+        lineHeight: cellSize,
+        marginTop: -1,
+        marginRight: -1,
+      }}
     ></div>
   );
 };
 
 type BoardProps = {
   board: Life.Board;
+  pattern: Life.Pattern;
   setCells: (pos: Array<[number, number]>, cell: Life.Cell) => void;
 };
 
-const Board: React.SFC<BoardProps> = ({ board, setCells }) => {
+const Board: React.SFC<BoardProps> = ({ board, pattern, setCells }) => {
   const [shadow, setShadow] = useState<Array<[number, number]>>([]);
-  const pattern = [[0, 0]];
 
   const handleMouseEnter = (x: number, y: number) => {
-    setShadow(pattern.map(([x2, y2]) => [x + x2, y + y2]));
+    setShadow(pattern.pattern.map(([px, py]) => [x + px, y + py]));
   };
 
   const boardElem = [];
@@ -77,13 +76,16 @@ const Board: React.SFC<BoardProps> = ({ board, setCells }) => {
         <Cell
           key={x}
           cell={board.getCell(x, y)}
-          shadow={shadow.some(([x2, y2]) => x === x2 && y === y2)}
+          shadow={shadow.some(([sx, sy]) => x === sx && y === sy)}
           setCells={(leftClick) => {
             if (!leftClick) {
               setCells([[x, y]], false);
               return;
             }
-            const pos: Array<[number, number]> = pattern.map(([x2, y2]) => [x + x2, y + y2]);
+            const pos: Array<[number, number]> = pattern.pattern.map(([px, py]) => [
+              x + px,
+              y + py,
+            ]);
             setCells(pos, true);
           }}
           handleMouseMove={() => handleMouseEnter(x, y)}
@@ -91,7 +93,14 @@ const Board: React.SFC<BoardProps> = ({ board, setCells }) => {
       );
     }
     boardElem.push(
-      <div key={y} className="board-row">
+      <div
+        key={y}
+        className="board-row"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
         {row}
       </div>
     );
@@ -102,8 +111,7 @@ const Board: React.SFC<BoardProps> = ({ board, setCells }) => {
       className="board"
       style={{
         display: "flex",
-        width: cellSize * (board.width + 1),
-        height: cellSize * (board.height + 1),
+        flexDirection: "column",
       }}
       onContextMenu={(e) => e.preventDefault()}
     >
@@ -113,9 +121,10 @@ const Board: React.SFC<BoardProps> = ({ board, setCells }) => {
 };
 
 const App = () => {
-  const [board, setBoard] = useState(new Life.Board());
+  const [board, setBoard] = useState(new Life.Board(20, 20));
   const [timerID, setTimerID] = useState<NodeJS.Timeout | undefined>(undefined);
   const [runInterval, setRunInterval] = useState(50);
+  const [pattern, setPattern] = useState(Life.patterns[0]);
 
   const setCells = (pos: Array<[number, number]>, cell: Life.Cell) => {
     setBoard((board) => {
@@ -135,7 +144,7 @@ const App = () => {
 
   const run = () => {
     if (timerID !== undefined) return;
-    setTimerID(setInterval(step, runInterval));
+    setTimerID(setInterval(step, runInterval) as NodeJS.Timeout);
   };
 
   const stop = () => {
@@ -157,27 +166,58 @@ const App = () => {
 
   return (
     <div>
-      <Board board={board} setCells={setCells} />
-      <div>
-        <button onClick={step}>step</button>
-        <button disabled={timerID !== undefined} onClick={run}>
-          run
-        </button>
-        <button disabled={timerID === undefined} onClick={stop}>
-          stop
-        </button>
-        <button onClick={clear}>clear</button>
-        <input
-          type="range"
-          min="10"
-          max="500"
-          step="10"
-          value={runInterval}
-          disabled={timerID !== undefined}
-          onChange={(e) => setRunInterval(Number(e.target.value))}
-        />
-        <span>{runInterval}(ms)</span>
-        <span>{board.generation}世代</span>
+      <div className="container_app" style={{ display: "flex", justifyContent: "center" }}>
+        <div
+          className="container_board_buttons"
+          style={{ display: "flex", flexDirection: "column", margin: 5 }}
+        >
+          <Board board={board} pattern={pattern} setCells={setCells} />
+          <div
+            className="conainer_buttons"
+            style={{ display: "flex", justifyContent: "center", margin: 5 }}
+          >
+            <button onClick={step}>step</button>
+            <button disabled={timerID !== undefined} onClick={run}>
+              run
+            </button>
+            <button disabled={timerID === undefined} onClick={stop}>
+              stop
+            </button>
+            <button onClick={clear}>clear</button>
+            <input
+              type="range"
+              min="10"
+              max="500"
+              step="10"
+              value={runInterval}
+              disabled={timerID !== undefined}
+              onChange={(e) => setRunInterval(Number(e.target.value))}
+            />
+            <span>{runInterval}(ms)</span>
+            <span>{board.generation}世代</span>
+          </div>
+        </div>
+        <form className="pattern_list" style={{ margin: 5 }}>
+          <select
+            size={Life.patterns.length}
+            value={pattern.name}
+            onChange={(e) => {
+              const p = Life.patterns.find((p) => p.name === e.target.value);
+              if (p === undefined) {
+                // ???
+                console.error(`cannot find pattern ${e.target.value}`);
+                return;
+              }
+              setPattern(p);
+            }}
+          >
+            {Life.patterns.map((pattern, i) => (
+              <option key={i} value={pattern.name}>
+                {pattern.name}
+              </option>
+            ))}
+          </select>
+        </form>
       </div>
     </div>
   );
--- src/life.ts
+++ src/life.ts
@@ -3,16 +3,18 @@ export type Cell = boolean;
 export class Board {
   board: Array<Array<Cell>>;
   generation: number;
-  readonly width: number = 20;
-  readonly height: number = 20;
+  readonly width: number;
+  readonly height: number;
 
-  constructor() {
+  constructor(width: number = 20, height: number = 20) {
+    this.width = width;
+    this.height = height;
     this.board = Array.from(new Array(this.height), () => new Array(this.width).fill(false));
     this.generation = 0;
   }
 
   clone(): Board {
-    const b = new Board();
+    const b = new Board(this.width, this.height);
     for (let y = 0; y < this.height; y++) {
       for (let x = 0; x < this.width; x++) {
         b.setCell(x, y, this.getCell(x, y));
@@ -70,3 +72,77 @@ export class Board {
     this.generation = 0;
   }
 }
+
+export type Pattern = {
+  name: string;
+  pattern: Array<[number, number]>;
+};
+
+export const patterns: Array<Pattern> = [
+  { name: "cell", pattern: [[0, 0]] },
+  {
+    name: "glider",
+    pattern: [
+      [0, -1],
+      [1, 0],
+      [-1, 1],
+      [0, 1],
+      [1, 1],
+    ],
+  },
+  {
+    name: "spaceship",
+    pattern: [
+      [-2, -1],
+      [1, -1],
+      [2, 0],
+      [-2, 1],
+      [2, 1],
+      [-1, 2],
+      [0, 2],
+      [1, 2],
+      [2, 2],
+    ],
+  },
+  {
+    name: "glider gun",
+    pattern: [
+      [7, -4],
+      [5, -3],
+      [7, -3],
+      [-5, -2],
+      [-4, -2],
+      [3, -2],
+      [4, -2],
+      [17, -2],
+      [18, -2],
+      [-6, -1],
+      [-2, -1],
+      [3, -1],
+      [4, -1],
+      [17, -1],
+      [18, -1],
+      [-17, 0],
+      [-16, 0],
+      [-7, 0],
+      [-1, 0],
+      [3, 0],
+      [4, 0],
+      [-17, 1],
+      [-16, 1],
+      [-7, 1],
+      [-3, 1],
+      [-1, 1],
+      [0, 1],
+      [5, 1],
+      [7, 1],
+      [-7, 2],
+      [-1, 2],
+      [7, 2],
+      [-6, 3],
+      [-2, 3],
+      [-5, 4],
+      [-4, 4],
+    ],
+  },
+];
